95b9c861ac02592d42a8ff15564d78d0b2611984
Tyler Payne <154254574+Yhento@users.noreply.github.com>
2024-08-21 08:53:42 -0500

    finish assignment

--- js/practice.js
+++ js/practice.js
@@ -1,3 +1,5 @@
+
+
 const tictactoeboard = (function() {
 
     let rows = 3;
@@ -31,10 +33,16 @@ const tictactoeboard = (function() {
         }
     }
 
+    const clearBoard = () => {
+        board = board.map(row => row.map(() => ""));
+
+    }
+
     return {
         printBoard,
         getBoard,
-        plotPoint
+        plotPoint,
+        clearBoard
     }
 })();
 
@@ -49,7 +57,83 @@ function Player(playerName, playerToken) {
 }
 
 
+const ui = ( function() {
 
+    const createElement = (tag, class_name = '') => {
+        element = document.createElement(tag);
+        
+        if (class_name) {
+            element.classList.add(class_name);
+        }
+
+        return element;
+    }
+
+    let message = createElement('span', 'game-message');
+
+    document.querySelector('.players-message').append(message);
+
+    const getMessage = () => message;
+
+    const renderGameBoard = () => {
+        for(i=0; i < 3; i++){
+
+            for(j=0; j < 3; j++) {
+
+                game_square = createElement('div', 'game-square')
+                game_square.dataset.row = i;
+                game_square.dataset.column =j;
+
+
+                
+
+                document.querySelector('.game-wrapper').append(game_square);
+            }
+
+        }
+    }
+
+    const updateGameBoard = () => {
+        let gameboard = tictactoeboard.getBoard();
+        let game_squares = document.querySelectorAll(".game-square");
+        
+        game_squares.forEach( square => {
+
+            square.textContent = gameboard[square.dataset.row][square.dataset.column];
+
+            if (square.textContent !== '') {
+                square.textContent === 'X' ? square.style.color = 'blue' : square.style.color = 'red';
+            }
+
+        });
+
+   
+            
+        
+    }
+    const createResetButton = () => {
+
+        let reset_button = createElement('button', 'reset');
+        console.log(reset_button);
+        reset_button.textContent = "Restart Game"
+        reset_button.onclick = game.resetGame;
+        document.querySelector(".players-message").appendChild(reset_button);
+
+    }
+
+
+
+
+return {
+    getMessage,
+    renderGameBoard,
+    updateGameBoard,
+    createElement,
+    createResetButton
+}
+
+
+})();
 
 
 const game = ( function() {
@@ -59,12 +143,86 @@ const game = ( function() {
     let winner = 0;
     let tie = 0;
     let board = tictactoeboard;
+    let message = ui.getMessage();
+
+    const startGame = () => {
+        let playerOneTextBox = document.getElementById('playerone');
+        let playerTwoTextBox = document.getElementById('playertwo');
+        let player_message_area = document.querySelector('.players-message');
+        const formfields = document.querySelector('.players-message').querySelectorAll('.form-field');
+        const start_button = document.querySelector('.start_button');
+        
+        if (playerOneTextBox.value.trim() === '' || playerTwoTextBox.value.trim() === '') {
+
+            message.textContent = `Player Names' Cannot Be Blank!`
+            return;
+
+        }
+
+        player1_label = ui.createElement('div', 'player-label');
+        player2_label =  ui.createElement('div', 'player-label');
+
+        player1_label.textContent = `Player One: ${playerOneTextBox.value} is X`
+        player2_label.textContent = `Player Two: ${playerTwoTextBox.value} is O`
+
+        message.textContent = '';
+
+        player_message_area.append(player1_label, player2_label, message);
+
+        formfields.forEach(formfield => {
+
+            player_message_area.removeChild(formfield);
+
+        });
+
+        player_message_area.removeChild(start_button);
+
+        game.setPlayers(playerOneTextBox.value, playerTwoTextBox.value);
+        ui.renderGameBoard();
+        game.initializeControls();
+
+        
+    
+
+    }
+
+    const resetGame = () => {
+        const player_message_area = document.querySelector('.players-message');
+        const reset_button = document.querySelector('.reset');
+        board.clearBoard();
+        winner = 0;
+        tie = 0;
+        activePlayer = playerOne;
+        message.textContent = printMessage();
+        ui.updateGameBoard();
+
+        player_message_area.removeChild(reset_button);
+ 
+ 
+ 
+     }
+
+    
+
+    const initializeControls = () => {
+        game_squares = document.querySelectorAll('.game-square');
+
+        game_squares.forEach( square => {
+
+            square.addEventListener('click', (e) => {
+
+                playRound(square.dataset.row, square.dataset.column);
+                ui.updateGameBoard();
+
+            })
+        });
+    }
 
     const setPlayers = (oneName, twoName) => {
         playerOne = Player(oneName, "X");
         playerTwo = Player(twoName, "O");
         activePlayer = playerOne;
-        console.log(printMessage()); // Print the initial message
+        message.textContent = printMessage();
     }
 
     const switchActivePlayer = () => {
@@ -79,7 +237,6 @@ const game = ( function() {
 
     function checkWinner (gameboard){
         const array_of_results = gameboard.flat();
-        let winner = 0;
         const winning_lines = [
             [0, 1, 2],
             [3, 4, 5],
@@ -119,7 +276,7 @@ const game = ( function() {
 
       
             if (winner === 1 || tie === 1) {
-                console.log("This Game is Over! Please Start a New Game!")
+                
                 return;
             }
 
@@ -127,27 +284,31 @@ const game = ( function() {
             let invalid_move = board.plotPoint(row, column, getActivePlayer().token);
 
             if (invalid_move === 1) {
-                console.log(`${row} ${column} is an invalid move! Try again ${getActivePlayer().name}!`)
+                
+                message.textContent = `This spot is taken! Try again ${getActivePlayer().name}!`
                 return;
             }
-            board.printBoard();
+            
             
             let gameboard = board.getBoard();
              winner = checkWinner(gameboard);
              tie = checkTie(gameboard);
             
             if (winner === 1) {
-                console.log(`There is a winner. The winner is ${getActivePlayer().name}`);
+                message.textContent = `There is a winner. The winner is ${getActivePlayer().name}`
+                ui.createResetButton();
                 return;
             }
 
             if (tie === 1) {
-                console.log (`This game is a tie!`)
+                
+                message.textContent= "This game is a tie!"
+                ui.createResetButton();
                 return;
             }
            
             switchActivePlayer();
-            console.log(printMessage());
+            message.textContent = printMessage();
 
        
         
@@ -161,19 +322,18 @@ const game = ( function() {
     return {
         getActivePlayer,
         playRound, 
-        setPlayers
+        setPlayers,
+        initializeControls,
+        startGame,
+        resetGame
     }
 })();
 
-game.setPlayers('Tyler', 'Stella');
 
-game.playRound(0, 2);
-game.playRound(0, 0);
-game.playRound(1, 1);
-game.playRound(2, 1);
-game.playRound(2, 1);
-game.playRound(2, 2);
-game.playRound(2, 0);
-game.playRound(1, 0);
-game.playRound(1, 2);
-game.playRound(0, 1);
+
+
+
+
+
+
+
